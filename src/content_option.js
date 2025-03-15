d19d3fb47ff71bbe887a064059339c19ea4f2c8e
Anuragi Thapliya <60832911+athapliya@users.noreply.github.com>
2025-03-15 01:28:08 -0400

    init commit

---
+++ src/content_option.js
@@ -0,0 +1,130 @@
+const logotext = "ANURAGI";
+const meta = {
+    title: "Anuragi Thapliya",
+    description: "I'm Anuragi, a recent robotics engineering graduate.",
+};
+
+const introdata = {
+    title: "I’m Anuragi Thapliya",
+    animated: {
+        first: "I am a robotics engineer",
+        second: "I am curious about industrial automation",
+        third: "I want to learn about future robotic applications",
+    },
+    description: "Welcome to my portfolio! I am a recent robotics engineering graduate. I want to design intelligent sytems that can make a difference and be accessible to everyone.",
+    your_img_url: "/portfoliopics/headshot.jpg",
+};
+
+const dataabout = {
+    title: "About myself",
+    aboutme: "I graduated Worcester Polytechnic Institute (WPI) in Spring 2025 with a Bachelor of Science in Robotics Engineering and a minor in Computer Science.",
+};
+const worktimeline = [{
+        jobtitle: "Product Engineer Intern",
+        where: "Universal Robots",
+        date: "2024-2025",
+    },
+    {
+        jobtitle: "Reliability/Manufacturing Engineer Intern",
+        where: "Brooks Automation",
+        date: "2023",
+    },
+    {
+        jobtitle: "Crimson Key Campus Tour Manager",
+        where: "WPI",
+        date: "2022-2025",
+    },
+];
+
+const skills = [{
+        name: "Languages: Python, C/C++, Java, Javascript",
+        // value: 90,
+    },
+    {
+        name: "Tools: ROS/ROS2, PyTorch, Git/Github ",
+        // value: 85,
+    },
+    {
+        name: "Applications/Software: SolidWorks, MATLAB, VSCode, Jupyter, Salesforce, Jira",
+        // value: 80,
+    },
+    {
+        name: "Operating System: Windows, Linux, MacOS",
+        // value: 60,
+    },
+    {
+        name: "Manufacturing: 3D-Printing, Laser Cutting, Soldering",
+        // value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+const dataportfolio = [{
+        img: "/portfoliopics/full_assembly.png",
+        description: "Remote Center of Motion Surgical Manipulator",
+        link: "https://drive.google.com/file/d/1XEFWqnUQLnpc0eEm57h_PyZx2grmfWUz/view?usp=sharing",
+    },
+    {
+        img: "/portfoliopics/dynamic.jpg",
+        description: "Color-Based Object Sorting/Dynamic Tracking Arm",
+        link: "https://drive.google.com/file/d/19vNU_jZJeYx8PDrPFpqxykRbOVgsBmb_/view?usp=sharing",
+    },
+    {
+        img: "/portfoliopics/object_grasp_points.png",
+        description: "Identifying Stable Grasp Points for Irregular 3D Objects",
+        link: "https://drive.google.com/file/d/1FrUrukUaDD4MRoaB49Y7h9JnC85JY5B5/view?usp=sharing",
+    },
+    {
+        img: "/portfoliopics/Localization2.png",
+        description: "Autonomous Navigation and SLAM Implementation",
+        link: "https://drive.google.com/file/d/154NkjiijBfqEbjT5e1PxTBMgQComhhMB/view?usp=sharing",
+    },
+    {
+        img: "/portfoliopics/mapping.png",
+        description: "Aerial Mapping for Urban Planning",
+        link: "https://drive.google.com/file/d/1J9Br6niYkhl-N1c7oLLgRbY8HvkxY-Lq/view?usp=sharing",
+    },
+];
+
+const contactConfig = {
+    YOUR_EMAIL: "athapliya@wpi.edu",
+    YOUR_FONE: "(571) 424-3021",
+    // description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    // YOUR_SERVICE_ID: "service_id",
+    // YOUR_TEMPLATE_ID: "template_id",
+    // YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    // github: "https://github.com",
+    // facebook: "https://facebook.com",
+    linkedin: "https://www.linkedin.com/in/athapliya/",
+    // twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
